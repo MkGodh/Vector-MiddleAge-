f75c1848156f990b694b08db2a1553da5240f68f
MkaGodh <fenryrvla@yandex.ru>
2024-06-04 17:47:11 +0300

    Добавьте файлы проекта.

---
+++ Vector(MiddleAge)/Vector(MiddleAge).cpp
@@ -0,0 +1,36 @@
+﻿#include <iostream>
+#include <vector>
+
+using namespace std;
+
+vector<int> add(vector<int> vec, int val) {
+    vector<int> newVec(vec.size() + 1);
+    for (int i = 0; i < vec.size(); i++)
+        newVec[i] = vec[i];
+    newVec[vec.size()] = val;
+
+    return newVec;
+}
+
+int main() {
+
+    vector<int> vec = {};
+
+    int count = 0;
+    int age = 0;
+    int sum = 0;
+    while (age != -1) {
+        cout << "Please enter pacient age: ";
+        cin >> age;
+
+        if (age != -1) {
+            vec = add(vec, age);
+        }
+    }
+
+    for (int i = 0; i < vec.size(); i++) {
+        sum += vec[i];
+    }
+    sum /= vec.size();
+    cout << "Middle age is " << sum << " elements is " << vec.size();
+}
